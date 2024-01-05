6bc3d7e08226ef4bd7e930ded2ea0953a38a6eaf
theisaachome <65864160+theisaachome@users.noreply.github.com>
2024-01-06 01:24:19 +0630

    Created test cast for findAll Method Operation..

--- testing-repository-layer/src/test/java/com/isaachome/repository/EmployeeRepoTest.java
+++ testing-repository-layer/src/test/java/com/isaachome/repository/EmployeeRepoTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 @DataJpaTest
 class EmployeeRepoTest {
     @Autowired
@@ -30,5 +32,46 @@ class EmployeeRepoTest {
         assertThat(savedEmployee).isNotNull();
         assertThat(savedEmployee.getId()).isGreaterThan(0);
     }
+
+    // JUnit test for findAll employee operation
+    @Test
+    public  void givenEmployeeList_whenFindAll_thenEmployeeList(){
+        // given -> precondition or setup
+        var emp1 = Employee.builder()
+                .firstName("isaac")
+                .lastName("home")
+                .mail("isaachome@gmail.com")
+                .build();
+        var emp2 = Employee.builder()
+                .firstName("mercy")
+                .lastName("home")
+                .mail("mercyhome@gmail.com")
+                .build();
+
+        employeeRepo.saveAll(List.of(emp1,emp2));
+        // when -> action or the behaviour that we are going to test
+        List<Employee> employees= employeeRepo.findAll();
+
+        // then -> verify the output...
+        assertThat(employees).isNotNull();
+        assertThat(employees.size()).isEqualTo(2);
+    }
+ // JUnit test for getEmployee By ID operation..
+     @Test
+     public  void givenId_whenFindByEmployeeID_thenEmployee(){
+         // given -> precondition or setup
+         var emp1 = Employee.builder()
+                 .firstName("isaac")
+                 .lastName("home")
+                 .mail("isaachome@gmail.com")
+                 .build();
+         employeeRepo.save(emp1);
+         // when -> action or the behaviour that we are going to test
+         Employee savedEmp = employeeRepo.findById(emp1.getId()).get();
+         // then -> verify the output...
+         assertThat(savedEmp).isNotNull();
+     }
+
+
 
 }
